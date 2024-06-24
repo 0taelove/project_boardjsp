33f6470f82c248b2bf6953f79501b83c1498ebc3
0taelove <0taelove@gmailc.com>
2024-06-24 12:28:58 +0900

    컨트롤러 객체로 관리되는 객체 정의

--- src/main/java/org/choongang/main/controllers/MainController.java
+++ src/main/java/org/choongang/main/controllers/MainController.java
@@ -4,7 +4,7 @@ import org.choongang.global.config.annotations.Controller;
 import org.choongang.global.config.annotations.GetMapping;
 import org.choongang.global.config.annotations.RequestMapping;
 
-// 컨트롤러 객체로 관리되는 객체
+// 컨트롤러 객체로 관리되는 객체 정의
 @Controller
 @RequestMapping("/") // 기본값은 비어있는 형태
 public class MainController {
