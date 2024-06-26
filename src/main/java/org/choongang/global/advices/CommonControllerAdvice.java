aded6e531851b450f99aa536b53a0062e43719b5
0taelove <0taelove@gmailc.com>
2024-06-26 09:41:10 +0900

    관리자 기능 사전 준비 - 예외 처리 및 메세지, 페이지 이동 수정사항

--- src/main/java/org/choongang/global/advices/CommonControllerAdvice.java
+++ src/main/java/org/choongang/global/advices/CommonControllerAdvice.java
@@ -26,11 +26,15 @@ public class CommonControllerAdvice {
 
         e.printStackTrace();
 
+        // 정의한 예외
         if (e instanceof CommonException commonException) {
+
+            // 응답 코드
             int status = commonException.getStatus();
             response.setStatus(status);
 
             StringBuffer sb = new StringBuffer(1000);
+            // 자바 스크립트 형태로 얼러트 띄워줌
             if (e instanceof AlertException alertException) {
                 sb.append(String.format("alert('');", e.getMessage()));
             }
@@ -41,16 +45,21 @@ public class CommonControllerAdvice {
             }
 
             if (e instanceof AlertRedirectException alertRedirectException) {
+                // 타겟과 주소까지도 변경 가능
                 String target = alertRedirectException.getTarget();
                 String url = alertRedirectException.getRedirectUrl();
 
                 sb.append(String.format("%s.location.replace('%s');", target, url));
             }
 
+            // 스크립트를 실행할 수 있는 전용 html 태그
             if (!sb.isEmpty()) {
                 request.setAttribute("script", sb.toString());
                 return "commons/execute_script";
             }
+        } else {
+            // CommonException으로 정의한 예외가 아닌 경우 - 응답 코드 500
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
 
         return "errors/error";
